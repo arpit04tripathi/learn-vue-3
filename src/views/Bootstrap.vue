7bca1c67851f70a561f725e76693253c9bb6b619
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-01-31 13:22:24 +0000

    bootstrap view and component

---
+++ src/views/Bootstrap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>This is bootstrap view</h1>
+      <MyBootstrapIcons />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyBootstrapIcons from '../components/test/MyBootstrapIcons.vue'
+</script>
+
+<style>
+@media (min-width: 1024px) {
+.about {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+}
+</style>
