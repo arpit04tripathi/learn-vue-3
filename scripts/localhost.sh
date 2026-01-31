83570a962d415a04a7423e289ae33cbdf012b88e
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-01-31 12:48:47 +0000

    local

--- scripts/localhost.sh
+++ scripts/localhost.sh
@@ -41,10 +41,14 @@ case $1 in
     banner "start local dev"
     npm run dev;
     ;;
+  deploy)
+    npm run deploy
+    ;;
   *)
     echo "  git - sync latest from main"
     echo "  start - start local"
     echo "  install - install from package.json"
+    echo "  deploy - deploys to gh pages"
     ;;
 esac
 
