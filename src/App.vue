a2bfe21354dd9000245c9734141d2c3a7c19eedb
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-04 15:12:16 +0000

    use bootstrap template

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,28 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import HeaderComponent from '@/components/_layout/HeaderComponent.vue';
+import FooterComponent from '@/components/_layout/FooterComponent.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/bootstrap">Bootstrap</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="flex-wrapper">
+    <HeaderComponent />
+    <main class="container-fluid my-3 p-3 brdr">
+      <RouterView />
+    </main>
+    <FooterComponent class="mt-auto p-3 brdr" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.flex-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.brdr {
+  border: dashed red 1px;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>This project is created with frontend framework vue js 3 using its <a
+        href="https://cli.vuejs.org/guide/creating-a-project.html" target="_blank">CLI command</a></p>
+    <p>
+    <ul>
+      <li>Vue 3 - <a href="https://cli.vuejs.org/guide/creating-a-project.html" target="_blank"
+          rel="noopener noreferrer">create project</a>, <a href="https://vuejs.org/guide/introduction.html"
+          target="_blank" rel="noopener noreferrer">introduction</a></li>
+      <li>Bootstrap 5 - <a href="https://getbootstrap.com/docs/5.3/examples/navbars/" target="_blank">navbars</a></li>
+      <li>nvm - <a href="https://www.nvmnode.com/guide/introduction.html" target="_blank" rel="noopener noreferrer">node
+          version manager</a></li>
+    </ul>
+    </p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
+    /*
     min-height: 100vh;
     display: flex;
     align-items: center;
+    */
   }
 }
 </style>
